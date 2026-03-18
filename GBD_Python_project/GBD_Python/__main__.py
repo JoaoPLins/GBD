2ed50978d0dfe6f2a287deb5bf5058f9f0212abb
JoaoPLins <jpmlins96@hotmail.com>
2026-03-18 14:43:35 -0300

    mainloop started
    simple mainloop. will maybe import the menu from other project. maybe should add the pygame window to this part of the code, considereing.

--- GBD_Python_project/GBD_Python/__main__.py
+++ GBD_Python_project/GBD_Python/__main__.py
@@ -1,9 +1,21 @@
 # TODO: Update the main function to your needs or remove it.
 
 
+
 def main() -> None:
-    print("Start coding in Python today!")
+    print("main gameloopstarter")
+    gameStatus = 1
+    while gameStatus != 0:
+        if gameStatus == 2:
+            print("game running")
+        elif gameStatus == 1:
+            print("game menu")
+        else:
+            print("game quit")
+            gameStatus = 0
+        
 
 
 if __name__ == "__main__":
     main()
+
